f7eada7944e38e4ba202ddb34404e23829215856
jihoonko <jihoon1998@gmail.com>
2026-06-03 19:36:32 +0900

    AUTO_COMMIT at 2026-06-03 19:36

--- autocommit.bat
+++ autocommit.bat
@@ -1,8 +1,9 @@
 :loop
+	python ../newscript/get.py
 	git add .
 	set HOURMINUTE=%time:~0,5%
         git commit -m "AUTO_COMMIT at %date% %HOURMINUTE%"
 	git push origin master
-	TIMEOUT 300
+	TIMEOUT 60
 	
 goto loop
